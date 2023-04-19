3c1872411a9f4bc0970458487c60596b8f676417
Bill Omole <omolebill01@gmail.com>
2023-04-19 23:37:38 +0000

    100-await.js

--- 0x01-ES6_promise/100-await.js
+++ 0x01-ES6_promise/100-await.js
@@ -4,7 +4,7 @@ export default async function asyncUploadUser() {
   try {
     const photo = await uploadPhoto();
     const user = await createUser();
-    return { photo: photo, user: user };
+    return { photo, user };
   } catch (error) {
     return { photo: null, user: null };
   }
