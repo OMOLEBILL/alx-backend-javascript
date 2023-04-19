f5263d64ba4a43a413830861fcd34d472a6cbdeb
Bill Omole <omolebill01@gmail.com>
2023-04-19 18:07:54 +0000

    reset

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,7 +1,3 @@
 export default function handleResponseFromAPI(promise) {
-  promise.then(() => {
-    return { status: 200, body: 'success' };
-  }).catch(() => {
-    return Error();
-  }).finally(() => {console.log('Got a response from the API')});
+  promise.then(() => ({ status: 200, body: 'success' })).catch(() => Error()).finally(() => { console.log('Got a response from the API'); });
 }
