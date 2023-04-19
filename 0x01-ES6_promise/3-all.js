f5dcac12ef0f3617ec6ea6c4955ce69c9f750c23
Bill Omole <omolebill01@gmail.com>
2023-04-19 21:19:12 +0000

    added

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()]).then((values) => {
+  return Promise.all([uploadPhoto(), createUser()]).then((values) => {
     const { body } = values[0];
     const { firstName } = values[1];
     const { lastName } = values[1];
