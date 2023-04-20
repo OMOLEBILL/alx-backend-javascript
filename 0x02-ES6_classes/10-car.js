88921d5f46de05927f36bd15dc739919db78f3a6
Bill Omole <omolebill01@gmail.com>
2023-04-20 22:54:34 +0000

    all

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,14 +1,12 @@
 export default class Car {
   constructor(brand, motor, color) {
-    if (typeof (brand) !== 'string' || typeof (motor) !== 'string' || typeof (color) !== 'string') {
-      throw new TypeError('attributes should all be strings');
-    }
     this._brand = brand;
     this._motor = motor;
     this._color = color;
   }
 
   cloneCar() {
-    return new this.constructor(this._brand, this._motor, this._color);
+    const carClone = Object.assign(Object.create(Object.getPrototypeOf(this)), this);
+    return carClone;
   }
 }
