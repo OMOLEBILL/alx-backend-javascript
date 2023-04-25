fafa7f43af9a345205d6751a03972212f953a655
Bill Omole <omolebill01@gmail.com>
2023-04-25 22:09:34 +0000

    9-groceries_list.js

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,7 +1,9 @@
-export default function groceriesList () {
- const map = new Map();
- const objs = {Apples: 10, Tomatoes: 10, Pasta: 1, Rice: 1, Banana: 5};
- const keys = Object.keys(objs);
- keys.map((item) => map.set(item, objs[item]));
- return map;
+export default function groceriesList() {
+  const map = new Map();
+  const objs = {
+    Apples: 10, Tomatoes: 10, Pasta: 1, Rice: 1, Banana: 5,
+  };
+  const keys = Object.keys(objs);
+  keys.map((item) => map.set(item, objs[item]));
+  return map;
 }
