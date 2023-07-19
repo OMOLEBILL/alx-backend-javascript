de5132f86d2f430677b47dfe3a2b3fb69f9ba424
OMOLEBILL <105784385+OMOLEBILL@users.noreply.github.com>
2023-07-19 14:11:43 +0200

    Update 4-http.js

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,16 +1,8 @@
-#!/usr/bin/env node
+const http = require('http');
 
-const http = require("http");
-const port = 1245
-
+const port = 1245;
 const app = http.createServer((req, res) => {
-    res.setHeader("Content-Type", "text/plain");
-    res.statusCode = 200
-    res.end("Hello Holberton School!");
-})
-
-app.listen(port, () => {
-    console.log(`Server is listening on port ${port}`);
-});
-
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello Holberton School!');
+}).listen(port);
 module.exports = app;
