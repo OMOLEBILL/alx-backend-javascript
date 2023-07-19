61c8593ae4d0d831b591d8097bad145b46b718ef
OMOLEBILL <105784385+OMOLEBILL@users.noreply.github.com>
2023-07-19 15:30:04 +0200

    Update 3-payment.test.js

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,27 +1,21 @@
-const chai = require('chai');
 const sinon = require('sinon');
-const sendPaymentRequestToApi = require('./3-payment');
 const Utils = require('./utils');
+const sendPaymentRequestToApi = require('./3-payment');
+const expect = require('chai').expect;
 
-expect = chai.expect;
-describe('sendPaymentRequestToApi', function (){
-  let calculateNumber = {};
-  beforeEach(function() {
-    calculateNumber = sinon.spy(Utils, 'calculateNumber');
-  });
-  this.afterEach(function() {
-    calculateNumber.restore();
-  })
-  it('validates Utils function called once', function() {
-    sendPaymentRequestToApi(100, 200);
-    expect(calculateNumber.calledOnce).to.be.true;
-  });
-  it('validates arguments called in Utils function', function(){
-    sendPaymentRequestToApi(100, 200);
-    expect(calculateNumber.calledWith('SUM', 100, 200)).to.be.true;
-  });
-  it('validates return value of Utils function', function() {
-    sendPaymentRequestToApi(100, 200);
-    expect(calculateNumber.returnValues[0]).to.equal(300);
+describe("sendPaymentRequestToApi", function() {
+  it('Tests Util.calculateNumber used', function() {
+    const spy = sinon.spy(Utils, 'calculateNumber');
+    const consoleSpy = sinon.spy(console, 'log');
+    const a = 100, b = 20;
+    const result = sendPaymentRequestToApi(a, b);
+    
+    expect(consoleSpy.calledWithExactly('The total is: 120')).to.equal(true);
+    expect(consoleSpy.calledOnce).to.be.true;
+    expect(spy.withArgs('SUM', a, b).calledOnce).to.be.true;
+    expect(spy.calledWith('SUM', a, b)).to.be.true;
+    expect(console.log(`The total is: ${Utils.calculateNumber('SUM', a, b)}`)).to.equal(result);
+    spy.restore();
+    consoleSpy.restore();
   });
 });
