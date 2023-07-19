4174935024fdb47813ef71fec2cf4b666386ae48
OMOLEBILL <105784385+OMOLEBILL@users.noreply.github.com>
2023-07-19 14:01:47 +0200

    Create 4-http.js

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+const http = require("http");
+const port = 1245
+
+const app = http.createServer((req, res) => {
+    res.setHeader("Content-Type", "text/plain");
+    res.statusCode = 200
+    res.end("Hello Holberton School!");
+})
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
+
+module.exports = app;
